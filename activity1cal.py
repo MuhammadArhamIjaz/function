bd635e35c41b7ed73dfb3d4892555b75bfaedabf
MuhammadArhamIjaz <m.arham.ijaz@gmail.com>
2025-08-25 20:17:09 +0700

    Add files via upload

---
+++ activity1cal.py
@@ -0,0 +1,7 @@
+import datetime
+import calendar
+x=datetime.datetime.now()
+print(x)
+yy=2024
+mm=3
+print(calendar.month(yy,mm))
---
+++ date_time.py
@@ -0,0 +1,8 @@
+from datetime import date,time,datetime
+today=date.today()
+now=datetime.now()
+print("Today's date:",today)
+print("Current time:",now)
+print("Current year:",now.year)
+print("Current month:",now.month)
+print("Current day:",now.day)
