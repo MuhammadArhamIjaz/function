306f954617de4e4e4431fe61c291dde20b550bb5
MuhammadArhamIjaz <m.arham.ijaz@gmail.com>
2025-09-04 19:17:21 +0700

    Add files via upload

---
+++ count.py
@@ -0,0 +1,10 @@
+L=[4,5,1,2,9,7,10,8]
+print("orignal List:",L)
+count=0
+for i in L:
+    count+=1
+avg=count/len(L)
+print("sum= ",count)
+print("average= ",avg)
+print("Smallest element is:",L[0])
+print("Largestelement is:",L[-1])
